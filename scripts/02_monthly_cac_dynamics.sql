02a1f448897c777d8f6015dbe66d3a0c12ae27a7
Taras <tkubitskyi@gmail.com>
2026-05-25 17:11:51 +0200

    02_monthly_cac_dynamics.sql

--- scripts/02_monthly_cac_dynamics.sql
+++ scripts/02_monthly_cac_dynamics.sql
@@ -1,7 +1,7 @@
 WITH
   deduped_data AS (
     SELECT *
-    FROM `skelar-intensive-2026.homework_sql.marketing_ads_raw`
+    FROM `marketing_ads_raw`
     QUALIFY
       ROW_NUMBER() OVER (PARTITION BY ad_id, date ORDER BY timestamp DESC) = 1
   ),
